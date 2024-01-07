026a534f411819aeb793a475d32ec12d7a58bc9f
Admin <spo2jake11@gmail.com>
2024-01-07 17:34:41 +0800

    Merge some works in other commits

--- src/com/database/connection/databaseConnection.java
+++ src/com/database/connection/databaseConnection.java
@@ -5,43 +5,48 @@
 package com.database.connection;
 
 import com.admin.reserve.result.SearchResultFrame;
-import java.sql.Timestamp;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
  * @author Admin
  */
 public class databaseConnection {
+
     static Map<String, String> results = new HashMap<>();
     public static Connection con = null;
-    
-    public static void getCon(){
-        String url = "jdbc:mysql://localhost:3306/bsit2_1c";
+
+    public static void getCon() {
+        String url = "jdbc:mysql://localhost:3306/bsit2.1c";
         String user = "root";
         String password = "";
-        
-        try{
+
+        try {
             con = DriverManager.getConnection(url, user, password);
-            if(con != null){
+            if (con != null) {
                 //Commented out the popup
                 //JOptionPane.showMessageDialog(null, "Database has been connected");
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Error in the database connection " + e);
         }
     }
+
     //Static method used in SearchReserveFrame
-    public static void getSearchResult(String otp){
-        try{
+    public static void getSearchResult(String otp) {
+        try {
             // Use the connection details from databaseConnection class
             getCon();
 
@@ -49,11 +54,11 @@ public class databaseConnection {
             String query = "SELECT * FROM reservation_db WHERE reserve_code = ?";
 
             try (PreparedStatement statement = con.prepareStatement(query)) {
-                
+
                 // Set the parameter in the query
                 statement.setString(1, otp);
-                try (ResultSet resultSet = statement.executeQuery()){
-                    
+                try (ResultSet resultSet = statement.executeQuery()) {
+
                     // Execute the query
                     if (resultSet.next()) {
                         // Reservation with the entered OTP found
@@ -68,7 +73,7 @@ public class databaseConnection {
 
                         SearchResultFrame srf = new SearchResultFrame();
                         srf.setVisible(true);
-                        
+
                         System.out.println(results.toString());
                     } else {
                         // No reservation with the entered OTP found
@@ -80,28 +85,47 @@ public class databaseConnection {
             JOptionPane.showMessageDialog(null, e);
         }
     }
-    public Map<String, String> getResult(){
+
+    //method to return key and values from static map
+    public Map<String, String> getResult() {
         return results;
     }
-    public static boolean addMenu(String name, String detail, String category, String price){
+
+    public static boolean addMenu(String name, String detail, String category, String price, String image) {
         String query = "INSERT INTO menu_db (name, detail, category, price, image, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?)";
         getCon();
-        try(PreparedStatement prep = con.prepareStatement(query)){
+        try (PreparedStatement prep = con.prepareStatement(query)) {
             prep.setString(1, name);
             prep.setString(2, detail);
             prep.setString(3, category);
             prep.setInt(4, Integer.parseInt(price));
-            prep.setString(5, "Nothing");
-            
+            prep.setString(5, image);
+
             Timestamp time = new Timestamp(new Date().getTime());
             prep.setTimestamp(6, time);
             prep.setTimestamp(7, time);
-            
+
             int result = prep.executeUpdate();
             return result > 0;
-        }catch(SQLException e){
+        } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, e);
             return false;
         }
+    }
+
+    public static File imageChooser() {
+        JFileChooser file = new JFileChooser();
+        file.setCurrentDirectory(new File("."));
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Images", "png", "jpg", "jpeg");
+        file.addChoosableFileFilter(filter);
+        try {
+            int a = file.showSaveDialog(null);
+            if (a == JFileChooser.APPROVE_OPTION) {
+                File select = file.getSelectedFile();
+            }
+        } catch (NullPointerException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return file.getSelectedFile();
     }
 }
