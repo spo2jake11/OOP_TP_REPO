5ddd6234734ee1e1c64c9ad1c19eb276ddb98360
Admin <spo2jake11@gmail.com>
2024-01-08 21:26:19 +0800

    added function to update past reservation to late when running program

--- src/oop_tp_resto/OOP_TP_RESTO.java
+++ src/oop_tp_resto/OOP_TP_RESTO.java
@@ -6,6 +6,13 @@ package oop_tp_resto;
 
 import com.admin.home.AdminHomeFrame;
 import com.database.connection.databaseConnection;
+import static com.database.connection.databaseConnection.con;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+
 /**
  *
  * @author Admin
@@ -17,9 +24,33 @@ public class OOP_TP_RESTO {
      */
     public static void main(String[] args) {
         AdminHomeFrame homeObj = new AdminHomeFrame();
-        homeObj.show();
-        
+        homeObj.setVisible(true);
         databaseConnection.getCon();
+        Timestamp time = new Timestamp(new Date().getTime());
+        String query = "SELECT * FROM reservation_db WHERE status = 'Waiting'";
+        try (PreparedStatement statement = con.prepareStatement(query); ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                if (resultSet.getTimestamp("reserve_date").before(time)) {
+                    int id = resultSet.getInt("ID");
+                    setUpdate(id);
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
-    
+
+    private static void setUpdate(int id) {
+        String query = "UPDATE reservation_db SET status = ?, updated_at = ? WHERE ID = ?";
+        Timestamp time = new Timestamp(new Date().getTime());
+        try (PreparedStatement state = con.prepareStatement(query);) {
+            state.setString(1, "Late");
+            state.setTimestamp(2, time);
+            state.setInt(3, id);
+            state.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
 }
